be7134143e1214ba50eb96d9f5ed52b1205200de
LucasEfdl <lucasefdl@gmail.com>
2023-05-22 16:45:06 -0300

    feat: adição dos imputs para realizar uma reserva

--- JavaScript/js/quartos.js
+++ JavaScript/js/quartos.js
@@ -1,5 +1,22 @@
-let roomList = document.getElementById("roomList");
+// SELECIONANDO AS DIVS PRINCIPAIS DE INTERAÇÕES
+
+const makeReservations = document.getElementById("makeReservations");
+const deleteReservations = document.getElementById("deleteReservations");
+const listReservations = document.getElementById("listReservations");
+const seekReservations = document.getElementById("seekReservations");
+const editReservations = document.getElementById("editReservations");
+const availableRooms = document.getElementById("availableRooms");
+const numberOfGuests = document.getElementById("numberOfGuests");
+
+// OUTROS ELEMENTOS HTML
+const roomList = document.getElementById("roomList");
+
+// VARIAVEIS GLOBAIS
 let roomVector = [];
+let name = "";
+let document;
+let lenghtOfStay;
+let numberRoom;
 
 function captureRooms() {
   return fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json")
@@ -10,15 +27,18 @@ function captureRooms() {
     });
 }
 
-function showAvailableRooms(roomVector) {
-  console.log(roomVector);
-  roomVector.forEach((quarto) => {
-    roomList.innerHTML += `<li> <strong>Disponibilidade:</strong> ${quarto.disponibilidade} || <strong>Preço:</strong> ${quarto.preco} || <strong>Localização:</strong> ${quarto.localizacao}</li>`;
-  });
+function showAvailableRooms() {
+  for (let i = 0; i < roomVector.length; i++) {
+    roomList.innerHTML += `<li> <strong>Disponibilidade:</strong> ${roomVector[i].disponibilidade} || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao}</li>`;
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   captureRooms().then((roomVector) => {
     showAvailableRooms(roomVector);
   });
+});
+
+document.addEventListener("clcik", () => {
+  showAvailableRooms(roomVector);
 });
