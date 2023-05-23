b8522f5a366d03d22e75a2a140f695e460dc816a
LucasEfdl <lucasefdl@gmail.com>
2023-05-23 09:14:08 -0300

    comecei a fazer a ista de fato

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,5 +1,5 @@
-// SELECIONANDO AS DIVS PRINCIPAIS DE INTERAÇÕES
-
+// SELECIONANDO OS OBJETOS HTML QUE SERAM USADOS
+// ---- DIVS PRINCIPAIS
 const makeReservations = document.getElementById("makeReservations");
 const deleteReservations = document.getElementById("deleteReservations");
 const listReservations = document.getElementById("listReservations");
@@ -8,15 +8,52 @@ const editReservations = document.getElementById("editReservations");
 const availableRooms = document.getElementById("availableRooms");
 const numberOfGuests = document.getElementById("numberOfGuests");
 
-// OUTROS ELEMENTOS HTML
+// ---- OUTROS ELEMENTOS HTML
 const roomList = document.getElementById("roomList");
 
-// VARIAVEIS GLOBAIS
-let roomVector = [];
-let name = "";
-let document;
-let lenghtOfStay;
-let numberRoom;
+// OBJETOS USADOS NO CODIGO
+
+let room = {
+  availablity: "",
+  price: null,
+  location: "",
+  number: null,
+};
+
+let guest = {
+  nameGuest: "",
+  lengthOfStay: 0,
+  docGuest: 0,
+  roomGuest: 0,
+  next: null,
+};
+
+// ------------------------------- COLOCAR A VERIFIAÇÃO PARA QUANDO JA TIVER RESERVAS FEITAS
+let headList = null;
+
+//FUNÇÕES SOBRE A LISTA DE HOSPEDES
+
+function createNode() {
+  return guest;
+}
+
+function createReserve() {
+  const newNode = createNode();
+
+  if (!headList) {
+    //CAPUTAR OS VALORES DOS INPUTS E COLOCAR AQUI
+    newNode.nameGuest = "";
+    newNode.lengthOfStay = 3;
+    newNode.docGuest = 12354;
+    newNode.roomGuest = 4;
+    headList = newNode;
+  } else {
+    let current = headList;
+    //fazer o while de forma que insira de maneira ordenada.
+  }
+}
+
+// FUNÇÕES SOBRE OS QUARTOS DO HOTEL
 
 function captureRooms() {
   return fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json")
@@ -33,12 +70,14 @@ function showAvailableRooms() {
   }
 }
 
+// EVENTOS DISPARADOS NO CODIGO
+
 window.addEventListener("DOMContentLoaded", () => {
-  captureRooms().then((roomVector) => {
-    showAvailableRooms(roomVector);
+  captureRooms().then(() => {
+    showAvailableRooms();
   });
 });
 
 document.addEventListener("clcik", () => {
-  showAvailableRooms(roomVector);
+  showAvailableRooms();
 });
