0d9a7ea035bd501ecde9faedf84836709a8b6602
LucasEfdl <lucasefdl@gmail.com>
2023-05-27 15:24:11 -0300

    feat: função para criar a reserva funcionando corretamente

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -4,7 +4,7 @@ const deleteReservations = document.getElementById("deleteReservations");
 const listReservations = document.getElementById("listReservations");
 const seekReservations = document.getElementById("seekReservations");
 const editReservations = document.getElementById("editReservations");
-const availableRooms = document.getElementById("availableRooms");
+const allRooms = document.getElementById("allRooms");
 const numberOfGuests = document.getElementById("numberOfGuests");
 const checkRoom = document.getElementById("checkRoom");
 
@@ -31,6 +31,40 @@ const inputDuration = document.getElementById("lenghtOfStay");
 const inputResbosibleDoc = document.getElementById("resbosibleDoc");
 const inputCheckRoom = document.getElementById("inputCheckRoom");
 
+// FUNÇÕES ASSINCRONAS DO PROGRAMA
+function captureRooms() {
+  return fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json")
+    .then((response) => response.json())
+    .then((quartos) => {
+      roomVector = quartos.dados;
+      return roomVector;
+    });
+}
+
+// ----------------TA DANDO B.O.
+function writeToFile() {
+  const jsonData = JSON.stringify(roomVector);
+
+  fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonData,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na solicitação POST");
+      }
+      // A solicitação POST foi bem-sucedida
+      // Faça qualquer ação necessária após a atualização do arquivo JSON
+    })
+    .catch((error) => {
+      console.error("Erro na solicitação POST:", error);
+      // Lide com o erro adequadamente
+    });
+}
+
 // OBJETOS E VARIAVEIS GLOBAIS
 
 let room = {
@@ -42,6 +76,8 @@ let room = {
 
 let headList = null;
 
+let inputNumberRoom = null;
+
 // FUNÇÕES SOBRE A INTERFACE DO PROJETO
 
 function hideDivs() {
@@ -50,7 +86,7 @@ function hideDivs() {
   listReservations.style.display = "none";
   seekReservations.style.display = "none";
   editReservations.style.display = "none";
-  availableRooms.style.display = "none";
+  allRooms.style.display = "none";
   numberOfGuests.style.display = "none";
   checkRoom.style.display = "none";
 }
@@ -63,6 +99,7 @@ function createNode() {
     lengthOfStay: 0,
     docGuest: 0,
     roomGuest: 0,
+    numberRoom: 0,
     next: null,
   };
 }
@@ -74,6 +111,7 @@ function createReserve() {
   newNode.nameGuest = inputNameResponse.value;
   newNode.lengthOfStay = parseInt(inputDuration.value);
   newNode.docGuest = parseInt(inputResbosibleDoc.value);
+  newNode.numberRoom = inputNumberRoom;
 
   if (!headList || newNode.nameGuest.localeCompare(headList.nameGuest) < 0) {
     newNode.next = headList;
@@ -91,39 +129,42 @@ function createReserve() {
     current.next = newNode;
   }
 
+  inputNameResponse.value = "";
+  inputDuration.value = "";
+  inputResbosibleDoc.value = "";
   console.log(headList);
 }
 
-// FUNÇÕES SOBRE OS QUARTOS DO HOTEL - transformar essa função assicrona em uma fução sincrona
-function captureRooms() {
-  return fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json")
-    .then((response) => response.json())
-    .then((quartos) => {
-      roomVector = quartos.dados;
-      return roomVector;
-    });
-}
-
 function showAvailableRooms() {
-  roomList[0].innerHTML = ``;
   roomList[1].innerHTML = ``;
   for (let i = 0; i < roomVector.length; i++) {
     if (roomVector[i].disponibilidade == "DISPONIVEL") {
-      roomList[0].innerHTML += `<li> <strong>Disponibilidade:</strong> ${roomVector[i].disponibilidade} || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao} || <strong>Numero:</strong> ${roomVector[i].numero}</li>`;
-      roomList[1].innerHTML += `<li> <strong>Disponibilidade:</strong> ${roomVector[i].disponibilidade} || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao} || <strong>Numero:</strong> ${roomVector[i].numero}</li>`;
+      roomList[1].innerHTML += `<li> <strong>Disponibilidade:</strong> <span class="greenText">${roomVector[i].disponibilidade}</span> || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao} || <strong>Numero:</strong> ${roomVector[i].numero}</li>`;
+    }
+  }
+}
+
+function showAllRooms() {
+  roomList[0].innerHTML = ``;
+  for (let i = 0; i < roomVector.length; i++) {
+    if (roomVector[i].disponibilidade == "DISPONIVEL") {
+      roomList[0].innerHTML += `<li> <strong>Disponibilidade:</strong> <span class="greenText">${roomVector[i].disponibilidade}</span> || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao} || <strong>Numero:</strong> ${roomVector[i].numero}</li>`;
+    } else {
+      roomList[0].innerHTML += `<li> <strong>Disponibilidade:</strong> <span class="redText">${roomVector[i].disponibilidade}</span> || <strong>Preço:</strong> ${roomVector[i].preco} || <strong>Localização:</strong> ${roomVector[i].localizacao} || <strong>Numero:</strong> ${roomVector[i].numero}</li>`;
     }
   }
 }
 
 function verificationRoom() {
-  let numberRoom = parseInt(inputCheckRoom.value) - 1;
+  inputNumberRoom = parseInt(inputCheckRoom.value) - 1;
   inputCheckRoom.value = "";
-  if (numberRoom <= 19 && numberRoom >= 0) {
-    if (roomVector[numberRoom].disponibilidade == "OCUPADO") {
+  if (inputNumberRoom <= 19 && inputNumberRoom >= 0) {
+    if (roomVector[inputNumberRoom].disponibilidade == "OCUPADO") {
       alert("Perdão, mas este quarto ja esta ocupado!!");
       return;
     }
     hideDivs();
+    roomVector[inputNumberRoom].disponibilidade = "OCUPADO";
     makeReservations.style.display = "block";
     return;
   }
@@ -168,8 +209,8 @@ editarReserva.addEventListener("click", () => {
 });
 mostrarQuartos.addEventListener("click", () => {
   hideDivs();
-  availableRooms.style.display = "block";
-  showAvailableRooms();
+  allRooms.style.display = "block";
+  showAllRooms();
 });
 quantitativoHospedes.addEventListener("click", () => {
   hideDivs();
