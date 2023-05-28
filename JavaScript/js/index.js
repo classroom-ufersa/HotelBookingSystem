1ba9f9416c938b8affcfcf21dccc1c2a1de82a42
LucasEfdl <lucasefdl@gmail.com>
2023-05-28 11:27:13 -0300

    feat: add function listReservation

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -19,6 +19,9 @@ const quantitativoHospedes = document.getElementById("quantitativoHospedes");
 
 // ---- OUTROS ELEMENTOS HTML
 const roomList = document.getElementsByClassName("roomList");
+const bookingList = document.getElementById("bookingList");
+const qntHospedes = document.getElementById("qntHospedes");
+const quartosDispo = document.getElementById("quartosDispo");
 
 // ---- BUTTONS
 const btnMakeReservation = document.getElementById("sendResevation");
@@ -30,10 +33,13 @@ const inputNameResponse = document.getElementById("nameResponse");
 const inputDuration = document.getElementById("lenghtOfStay");
 const inputResbosibleDoc = document.getElementById("resbosibleDoc");
 const inputCheckRoom = document.getElementById("inputCheckRoom");
+const inputAmountPeople = document.getElementById("amountPeople");
 
 // FUNÇÕES ASSINCRONAS DO PROGRAMA
 function captureRooms() {
-  return fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json")
+  return fetch(
+    "http://127.0.0.1:5500/HotelBookingSystem/JavaScript/js/quartos.json"
+  )
     .then((response) => response.json())
     .then((quartos) => {
       roomVector = quartos.dados;
@@ -45,7 +51,7 @@ function captureRooms() {
 function writeToFile() {
   const jsonData = JSON.stringify(roomVector);
 
-  fetch("http://127.0.0.1:5500/JavaScript/js/quartos.json", {
+  fetch("http://127.0.0.1:5500/HotelBookingSystem/JavaScript/js/quartos.json", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -91,6 +97,30 @@ function hideDivs() {
   checkRoom.style.display = "none";
 }
 
+function amountGuests() {
+  let current = headList;
+  let cont = 0;
+  if (!current) {
+    qntHospedes.innerText = "Não há hospedes cadastrados no momento";
+    return;
+  }
+  while (current) {
+    cont += current.amountPeople;
+    current = current.next;
+  }
+  qntHospedes.innerText = `${cont}`;
+}
+
+function amountRooms() {
+  let cont = 0;
+  roomVector.forEach((element) => {
+    if (element.disponibilidade == "DISPONIVEL") {
+      cont++;
+    }
+  });
+  quartosDispo.innerText = `${cont}`;
+}
+
 //FUNÇÕES SOBRE A LISTA DE HOSPEDES
 
 function createNode() {
@@ -98,19 +128,19 @@ function createNode() {
     nameGuest: "",
     lengthOfStay: 0,
     docGuest: 0,
-    roomGuest: 0,
     numberRoom: 0,
+    amountPeople: 0,
     next: null,
   };
 }
 
-// falta: colocar o numero do quarto, porem para fazer isso deve-se fazer uma função que verifica se o quarto esta ocupado opu não
 function createReserve() {
   const newNode = createNode();
 
   newNode.nameGuest = inputNameResponse.value;
   newNode.lengthOfStay = parseInt(inputDuration.value);
   newNode.docGuest = parseInt(inputResbosibleDoc.value);
+  newNode.amountPeople = parseInt(amountPeople.value);
   newNode.numberRoom = inputNumberRoom;
 
   if (!headList || newNode.nameGuest.localeCompare(headList.nameGuest) < 0) {
@@ -132,6 +162,9 @@ function createReserve() {
   inputNameResponse.value = "";
   inputDuration.value = "";
   inputResbosibleDoc.value = "";
+  inputAmountPeople.value = "";
+  amountGuests();
+  amountRooms();
   console.log(headList);
 }
 
@@ -172,9 +205,11 @@ function verificationRoom() {
 }
 
 function listReservation() {
+  bookingList.innerHTML = ``;
   let current = headList;
   while (current) {
-    console.log(current.nameGuest);
+    bookingList.innerHTML += `<li><strong>Nome do Responsavel:</strong> ${current.nameGuest} || <strong>Documento:</strong> ${current.docGuest} || <strong>Duração da estadia:</strong> ${current.lengthOfStay} || <strong>Total de pessoas:</strong> ${current.amountPeople} || <strong>Número do quarto:</strong> ${current.numberRoom}</li>`;
+    console.log(current);
     current = current.next;
   }
 }
@@ -182,7 +217,10 @@ function listReservation() {
 // EVENTOS DISPARADOS NO CODIGO
 
 window.addEventListener("DOMContentLoaded", () => {
-  captureRooms();
+  captureRooms().then(() => {
+    amountRooms();
+  });
+  amountGuests();
 });
 
 realizarReserva.addEventListener("click", () => {
@@ -220,6 +258,7 @@ quantitativoHospedes.addEventListener("click", () => {
 btnMakeReservation.addEventListener("click", (event) => {
   event.preventDefault();
   createReserve();
+  hideDivs();
 });
 
 btnStartRegisterstar.addEventListener("click", () => {
